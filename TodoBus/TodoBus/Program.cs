1887edc600285b31e3ec652715fc2625ef210df3
DiegoLem <diego.lem077@gmail.com>
2020-02-09 16:46:56 -0600

    Formulario de Login
    - Creación de formulario

--- TodoBus/TodoBus/Program.cs
+++ TodoBus/TodoBus/Program.cs
@@ -16,7 +16,7 @@ namespace TodoBus
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Menu());
+            Application.Run(new Login());
         }
     }
 }
