43d7eaba890957fee9bcbeef34dc2d89c893f062
DiegoLem <diego@ese.plus>
2020-02-26 21:02:02 -0600

    Refactor SetupProject

--- TodoBus/Properties/AssemblyInfo.cs
+++ TodoBus/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("HP Inc.")]
 [assembly: AssemblyProduct("TodoBus")]
-[assembly: AssemblyCopyright("Copyright © HP Inc. 2020")]
+[assembly: AssemblyCopyright("Copyright ©2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
