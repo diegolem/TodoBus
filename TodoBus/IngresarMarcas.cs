411c69598850ff07e978445f6282380d2c2ba5ae
Vladimir <vladimir20sth@gmail.com>
2020-03-08 12:16:31 -0600

    Marcas y tipos de repuestos

---
+++ TodoBus/IngresarMarcas.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TodoBus.Views.SpareCategoriesSubClasses;
+using TodoBus.Views.Units;
+using TodoBus.Views.Users;
+
+namespace TodoBus
+{
+    public partial class IngresarMarcas : Form
+    {
+        public IngresarMarcas()
+        {
+            InitializeComponent();
+        }
+
+        private void bunifuImageButton1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void bunifuImageButton2_Click(object sender, EventArgs e)
+        {
+            if (Options.Width == 50)
+            {
+                Options.Visible = false;
+                Options.Width = 215;
+                PanelTransition.ShowSync(Options);
+                LogoTransition.ShowSync(logo);
+            }
+            else
+            {
+                LogoTransition.Hide(logo);
+                Options.Visible = false;
+                Options.Width = 50;
+                PanelTransition.ShowSync(Options);
+            }
+        }
+
+        private void btnLogOut_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Login frmLogin = new Login();
+            frmLogin.Show();
+        }
+
+        private void btnClients_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Clientes frmClientes = new Clientes();
+            frmClientes.Show();
+        }
+
+        private void bunifuFlatButton1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            SubCategories frmSub = new SubCategories();
+            frmSub.Show();
+        }
+
+        private void bunifuImageButton2_Click_1(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void btnUserConfig_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            DatosUsuario frmConfig = new DatosUsuario();
+            frmConfig.Show();
+        }
+
+        private void btnUnits_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Unidades frmUnits = new Unidades();
+            frmUnits.Show();
+        }
+
+        private void btnSubClase_Click(object sender, EventArgs e)
+        {
+            SubClasses frmSub = new SubClasses();
+            frmSub.Show();
+            this.Hide();
+        }
+    }
+}
