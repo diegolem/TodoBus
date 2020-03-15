3c8886a3e84c61bc25286172373bca37513b6ff4
Byron Amaya <byronamaya96@gmail.com>
2020-03-15 14:31:30 -0600

    RETURN brands
    :(

--- TodoBus/Controllers/BrandController.cs
+++ TodoBus/Controllers/BrandController.cs
@@ -3,10 +3,80 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TodoBus.Models;
 
 namespace TodoBus.Controllers
 {
     class BrandController
     {
+        public bool save(string name, string descript)
+        {
+            //Abro conexion solamente cuando ejecute la accion
+            using (TodoBusEntities db = new TodoBusEntities())
+            {
+                try
+                {
+                    //Defino el nuevo objeto
+                    brands Marca = new brands();
+                    //Luego obtengo todos los valores y los asigno a los campos del nuevo objeto
+                    Marca.name = name;
+                    Marca.description = descript;
+
+                    //Añado a mi tabla la subcategoria(objeto)
+                    db.brands.Add(Marca);
+                    //Guardo los cambios para confirmar
+                    db.SaveChanges();
+                    //Si todo bien regreso true
+                    return true;
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+        }
+
+        public List<brands> getAllBrands()
+        {
+            using (TodoBusEntities db = new TodoBusEntities())
+            {
+                //Obtengo todos los registros de mi tabla en la variable lst
+                var lst = from d in db.brands
+                          select d;
+                //Luego colocamos los registros que generamos de la base en el DataGridView y lo pasamos a lista para que
+                //sea compatible con el DGV
+                if (lst.Count() > 0)
+                {
+                    return lst.ToList();
+                }
+                else
+                {
+                    List<brands> newBR = new List<brands>();
+                    return newBR;
+                }
+            }
+        }
+
+        public bool delete(int? id)
+        {
+            using (TodoBusEntities db = new TodoBusEntities())
+            {
+                try
+                {
+                    //Se obtiene el objeto a borrar
+                    brands brdn = db.brands.Find(id);
+                    //Se borra el objeto que se obtuvo de la tabla
+                    db.brands.Remove(brdn);
+                    //Guardamos cambios
+                    db.SaveChanges();
+                    //Si todo bien regreso true
+                    return true;
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+        }
     }
 }
