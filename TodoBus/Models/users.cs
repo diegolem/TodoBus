220248fe9e0bcd5424cb3909191a1ae3bb722b86
DiegoLem <diego@ese.plus>
2020-04-26 14:17:22 -0600

    Campo de token para reestablecer contraseña

--- TodoBus/Models/users.cs
+++ TodoBus/Models/users.cs
@@ -25,6 +25,7 @@ namespace TodoBus.Models
         public string last_name { get; set; }
         public string email { get; set; }
         public Nullable<int> age { get; set; }
+        public int token { get; set; }
         public string password { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
